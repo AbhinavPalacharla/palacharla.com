8a8f102faaca9da36872f8e0c233ded5efc36edc
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2023-06-24 22:39:55 -0400

    ✨ Completed footer with clock

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,64 @@
+import { useEffect, useState } from "react";
+import { Roboto_Mono } from "next/font/google";
+
+const robotoMono = Roboto_Mono({
+  weight: ["300"],
+  subsets: ["latin"],
+  style: ["italic"],
+});
+
 const Footer: React.FC = () => {
+  const [time, setTime] = useState({ hour: "0", minute: "0", second: "0" });
+  const [blink, setBlink] = useState(false);
+
+  useEffect(() => {
+    const interval1 = setInterval(() => {
+      const time = new Date(
+        new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
+      );
+
+      const hour =
+        time.getHours().toLocaleString().length == 1
+          ? "0" + time.getHours().toLocaleString()
+          : time.getHours().toLocaleString();
+      // time.getHours().toString();
+      const minute =
+        time.getMinutes().toLocaleString().length == 1
+          ? "0" + time.getMinutes().toLocaleString()
+          : time.getMinutes().toLocaleString();
+      const second =
+        time.getSeconds().toLocaleString().length == 1
+          ? "0" + time.getSeconds().toLocaleString()
+          : time.getSeconds().toLocaleString();
+      setTime({ hour, minute, second });
+    }, 0);
+
+    const interval2 = setInterval(() => {
+      setBlink(!blink);
+    }, 500);
+
+    return () => {
+      clearInterval(interval1);
+      clearInterval(interval2);
+    };
+  }, [blink, setBlink]);
+
   return (
-    <div className="flex justify-between text-sm font-light pb-6 pt-16 px-36 bg-[#1C1C1C]">
-      <h1>2023 © Abhinav Palacharla</h1>
-      <h1>17:31:32 — San Diego, CA</h1>
+    <div className="flex items-center justify-between text-sm pb-6 pt-16 px-36 bg-[#1C1C1C]">
+      <h1 className="font-light">2023 © Abhinav Palacharla</h1>
+
+      <div className="flex flex-row">
+        <div
+          className={`flex flex-row items-center gap-x-0.5 ${robotoMono.className} w-20`}
+        >
+          <h1>{time.hour}</h1>
+          <h1 className={` ${blink ? "text-[#A4A4A5]" : ""}`}>:</h1>
+          <h1>{time.minute}</h1>
+          <h1 className={` ${blink ? "text-[#A4A4A5]" : ""}`}>:</h1>
+          <h1>{time.second}</h1>
+        </div>
+        <h1 className={robotoMono.className}>— San Diego, CA</h1>
+      </div>
     </div>
   );
 };
