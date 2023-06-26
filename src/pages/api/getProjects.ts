8dca67165902cb7895600db4e014e6561f91a27f
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2023-06-26 19:56:03 +0200

    ✨ Backend API route for getting projects

---
+++ src/pages/api/getProjects.ts
@@ -0,0 +1,60 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { Client } from "@notionhq/client";
+import type {
+  QueryDatabaseResponse,
+  PageObjectResponse,
+  PartialPageObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints";
+
+//get around type errors from Notion API
+interface DatabaseResponse extends Omit<QueryDatabaseResponse, "results"> {
+  results: (
+    | PageObjectResponse
+    | (PartialPageObjectResponse & { properties: any })
+  )[];
+}
+
+type Project = {
+  name: string;
+  description: string;
+  source_link: string;
+  image_links: string[];
+  technologies: string[];
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+
+  const database_id = process.env.NOTION_DATABASE_ID as string;
+
+  const response = (await notion.databases.query({
+    database_id: database_id,
+  })) as Partial<DatabaseResponse>;
+
+  const rows = response.results!.map((result) => result.properties);
+
+  const projects: Array<Project> = rows.map((row) => {
+    return {
+      name: row.Name.title[0].type,
+      description: row.description.rich_text[0].plain_text,
+      source_link: row.source_link.url,
+      image_links: row.image_links.rich_text
+        .map((text: any) => {
+          return text.href;
+        })
+        .filter((link: any) => {
+          if (link != null) {
+            return link;
+          }
+        }),
+      technologies: row.technologies.rich_text[0].plain_text.split(", "),
+    };
+  });
+
+  res.status(200).json(projects);
+};
+
+export default handler;
