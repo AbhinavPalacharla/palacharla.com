b42ecfd16a3f595dfb393edef8cebca217c75588
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2023-06-28 17:56:19 +0200

    🛠 Minor visual changes to about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,28 +14,28 @@ const Index: NextPage = () => {
     <div className="flex flex-col">
       <div className="flex flex-col gap-y-6 lg:gap-y-8 font-light tracking-wide text-sm lg:text-base">
         <h1 className="leading-relaxed">
-          Hey, I&apos;m <i>Abhinav</i>. I&apos;m a student developer and
-          designer based in San Diego, CA.
+          Hey, I&apos;m <i className="font-serif">Abhinav</i>. I&apos;m a
+          student developer and designer based in San Diego, CA.
         </h1>
 
-        <h1 className="sm: visible lg:invisible lg:hidden sm:leading-relaxed lg:leading-normal">
+        <h1 className="sm:visible lg:invisible lg:hidden sm:leading-relaxed lg:leading-normal">
           Previously: developed AI integrated solutions at <u>Okulis</u>, Web3
           interfaces at <u>Mintra</u> and <u>Unsung</u>, and blockchain based
           protocols at <u>SDSC</u>.
         </h1>
-        <h1 className="sm: hidden lg:visible lg:inline-block">
-          Previously: developed AI integrated solutions at <u>Okulis</u>, Web3
+        <h1 className="sm:hidden lg:visible lg:inline-block">
+          Previously developed AI integrated solutions at <u>Okulis</u>, Web3
           interfaces at <u>Mintra</u> and <u>Unsung</u>, and <br /> blockchain
           based protocols at <u>SDSC</u>.
         </h1>
 
-        <h1 className="sm: visible lg:invisible lg:hidden leading-relaxed">
+        <h1 className="sm:visible lg:invisible lg:hidden leading-relaxed">
           Currently developing web apps with{" "}
           <i>Next.js + Typescript + TailwindCSS</i>, exploring design with{" "}
           <i>Figma</i>, and dabbling with AI integration through <i>OpenAI</i>.
         </h1>
 
-        <h1 className="sm: hidden lg:visible lg:inline-block">
+        <h1 className="sm:hidden lg:visible lg:inline-block">
           Currently developing web apps with{" "}
           <i>Next.js + Typescript + TailwindCSS</i>, exploring design with
           <br /> <i>Figma</i>, and dabbling with AI integration through{" "}
@@ -47,7 +47,7 @@ const Index: NextPage = () => {
         </h1>
       </div>
       <Divider className="pt-8 lg:pt-16 pb-4 lg:pb-8" />
-      <div className="flex flex-row items-center gap-x-4 lg:gap-x-12 italic font-light text-sm lg:text-base">
+      <div className="flex flex-row items-center gap-x-4 lg:gap-x-12 italic font-light text-sm lg:text-base tracking-wider">
         <Link className="hover:underline" href="">
           LinkedIn
         </Link>
