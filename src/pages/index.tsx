c8104e52969e62543b1b35b28e2e41b107383935
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2023-07-05 17:05:31 -0400

    ✨ Subtle animation to about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { motion } from "framer-motion";
 
 const Index: NextPage = () => {
   const { isLoading, isError, data } = useQuery(["projects"], async () => {
@@ -13,41 +14,82 @@ const Index: NextPage = () => {
   return (
     <div className="flex flex-col">
       <div className="flex flex-col gap-y-6 lg:gap-y-8 font-light tracking-wide text-sm lg:text-base">
-        <h1 className="leading-relaxed">
+        <motion.h1
+          className="leading-relaxed"
+          initial={{ opacity: 0 }}
+          transition={{ delay: 0.2 }}
+          animate={{ opacity: 1 }}
+        >
           Hey, I&apos;m <i className="font-serif">Abhinav</i>. I&apos;m a
           student developer and designer based in San Diego, CA.
-        </h1>
+        </motion.h1>
 
-        <h1 className="sm:visible lg:invisible lg:hidden sm:leading-relaxed lg:leading-normal">
+        <motion.h1
+          className="sm:visible lg:invisible lg:hidden sm:leading-relaxed lg:leading-normal"
+          initial={{ opacity: 0 }}
+          transition={{ delay: 0.25 }}
+          animate={{ opacity: 1 }}
+        >
           Previously: developed AI integrated solutions at <u>Okulis</u>, Web3
           interfaces at <u>Mintra</u> and <u>Unsung</u>, and blockchain based
           protocols at <u>SDSC</u>.
-        </h1>
-        <h1 className="sm:hidden lg:visible lg:inline-block">
+        </motion.h1>
+        <motion.h1
+          className="sm:hidden lg:visible lg:inline-block"
+          initial={{ opacity: 0 }}
+          transition={{ delay: 0.25 }}
+          animate={{ opacity: 1 }}
+        >
           Previously developed AI integrated solutions at <u>Okulis</u>, Web3
           interfaces at <u>Mintra</u> and <u>Unsung</u>, and <br /> blockchain
           based protocols at <u>SDSC</u>.
-        </h1>
+        </motion.h1>
 
-        <h1 className="sm:visible lg:invisible lg:hidden leading-relaxed">
+        <motion.h1
+          className="sm:visible lg:invisible lg:hidden leading-relaxed"
+          initial={{ opacity: 0 }}
+          transition={{ delay: 0.3 }}
+          animate={{ opacity: 1 }}
+        >
           Currently developing web apps with{" "}
           <i>Next.js + Typescript + TailwindCSS</i>, exploring design with{" "}
           <i>Figma</i>, and dabbling with AI integration through <i>OpenAI</i>.
-        </h1>
+        </motion.h1>
 
-        <h1 className="sm:hidden lg:visible lg:inline-block">
+        <motion.h1
+          className="sm:hidden lg:visible lg:inline-block"
+          initial={{ opacity: 0 }}
+          transition={{ delay: 0.3 }}
+          animate={{ opacity: 1 }}
+        >
           Currently developing web apps with{" "}
           <i>Next.js + Typescript + TailwindCSS</i>, exploring design with
           <br /> <i>Figma</i>, and dabbling with AI integration through{" "}
           <i>OpenAI</i>.
-        </h1>
+        </motion.h1>
 
-        <h1 className="sm:leading-relaxed lg:leading-normal">
+        <motion.h1
+          className="sm:leading-relaxed lg:leading-normal"
+          initial={{ opacity: 0 }}
+          transition={{ delay: 0.35 }}
+          animate={{ opacity: 1 }}
+        >
           Feel free to reach out to me on any of my socials.
-        </h1>
+        </motion.h1>
       </div>
-      <Divider className="pt-8 lg:pt-16 pb-4 lg:pb-8" />
-      <div className="flex flex-row items-center gap-x-4 lg:gap-x-12 italic font-light text-sm lg:text-base tracking-wider">
+      <motion.div
+        initial={{ opacity: 0 }}
+        transition={{ delay: 0.4 }}
+        animate={{ opacity: 1 }}
+      >
+        <Divider className="pt-8 lg:pt-16 pb-4 lg:pb-8" />
+      </motion.div>
+      <motion.div
+        className="flex flex-row items-center gap-x-4 lg:gap-x-12 italic font-light text-sm lg:text-base tracking-wider"
+        initial={{ opacity: 0 }}
+        transition={{ delay: 0.4 }}
+        animate={{ opacity: 1 }}
+      >
         <Link className="hover:underline" href="">
           LinkedIn
         </Link>
@@ -63,7 +105,7 @@ const Index: NextPage = () => {
         <Link className="hover:underline" href="">
           Twitter
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 };
